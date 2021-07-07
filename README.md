2908f56536ecba84fe2d35eb654be842f26e2dd1
Dev <congminh.le@gosu.vn>
2021-07-07 16:04:06 +0700

    UP

---
+++ README.md
@@ -0,0 +1,7 @@
+# What is this?
+
+Get alert javascript
+
+# Installation
+
+`npm i test-theme --save`
---
+++ index.js
@@ -0,0 +1,6 @@
+function showalert(){
+	alert('alert demo');
+}
+
+
+module.exports.showalert = showalert;
